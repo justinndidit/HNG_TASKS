327a5acb4bdd9fca69d3252023fc43daa5258f88
justinndidit <favourolasoji@student.oauife.edu.ng>
2023-09-07 18:54:03 +0100

    Formatted the dates correctly

--- task_1/data.js
+++ task_1/data.js
@@ -13,7 +13,8 @@ module.exports = [
     slack_name: "Olasoji Favour",
     current_day: daysOfWeek[new Date().getDay()],
     //utc_time: new Date().toISOString().replace(/\.\d{3}Z$/, "Z"),
-    utc_time: new Date().toISOString().slice(0, -5) + "Z",
+    utc_time: new Date().toISOString().slice(0, 19) + "Z",
+
     track: "backend",
     github_file_url:
       "https://github.com/justinndidit/HNG_TASKS/blob/main/task_1/server.js",
