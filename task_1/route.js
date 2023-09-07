df5731e9eebcf55079de3b0c3f4c391d8239d4e6
justinndidit <favourolasoji@student.oauife.edu.ng>
2023-09-07 19:39:32 +0100

    Changed project structure

--- task_1/route.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const router = new Router();
-
-const getUsers = require("./controller");
-
-router.get("/", getUsers);
-
-module.exports = router;
--- task_1/server.js
+++ task_1/server.js
@@ -1,10 +1,43 @@
 const express = require("express");
 const app = express();
-const router = require("./route");
+
+const daysOfWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const Data = [
+  {
+    slack_name: "Olasoji Favour",
+    current_day: daysOfWeek[new Date().getDay()],
+    utc_time: new Date().toISOString().slice(0, -5) + "Z",
+    track: "backend",
+    github_file_url:
+      "https://github.com/justinndidit/HNG_TASKS/blob/main/task_1/server.js",
+    github_repo_url: "https://github.com/justinndidit/HNG_TASKS",
+    status_code: 200,
+  },
+];
 
 const PORT = 8000;
 
-app.use("/api", router);
+app.get("/api", (req, res) => {
+  const { slack_name, track } = req.query;
+
+  const userData = Data.find(function (data) {
+    return (
+      data.slack_name.toLowerCase().split(" ").join("_") === slack_name &&
+      data.track === track
+    );
+  });
+
+  res.status(200).json(userData);
+});
 app.use("*", (req, res) => {
   res.status(404).send(`Route ${req.url} not found`);
 });
