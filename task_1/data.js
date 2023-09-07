ef19147ddd9ff52e2f63ae94c7f5a3906ca6aed0
justinndidit <favourolasoji@student.oauife.edu.ng>
2023-09-07 17:50:01 +0100

    Formatted the dates correctly

--- task_1/data.js
+++ task_1/data.js
@@ -12,7 +12,7 @@ module.exports = [
   {
     slack_name: "Olasoji Favour",
     current_day: daysOfWeek[new Date().getDay()],
-    utc_time: new Date(),
+    utc_time: new Date().toISOString().split(".")[0] + "z",
     track: "backend",
     github_file_url:
       "https://github.com/justinndidit/HNG_TASKS/blob/main/task_1/server.js",
@@ -20,3 +20,15 @@ module.exports = [
     status_code: 200,
   },
 ];
+
+// function formatDate() {
+//   // Create a new Date object with the current date and time
+//   const currentDate = new Date();
+
+//   // Remove milliseconds
+//   currentDate.setMilliseconds(0);
+
+//   // Convert the date to the ISO string format
+//   const date = currentDate.toISOString().toString().replace(".000", "");
+//   return new Date(date);
+// }
