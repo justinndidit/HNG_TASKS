ed1349d1457f0fccd80c4b30a621a83df12c3aed
justinndidit <favourolasoji@student.oauife.edu.ng>
2023-09-07 12:55:10 +0100

    HNG Task 1 completed

--- task_1/data.js
+++ task_1/data.js
@@ -2,7 +2,7 @@ module.exports = [
   {
     slack_name: "Olasoji Favour",
     current_day: new Date().getDay(),
-    utc_time: new Date().getUTCDate(),
+    utc_time: new Date(),
     track: "backend",
     github_file_url: "https://github.com/username/repo/blob/main/file_name.ext",
     github_repo_url: "https://github.com/username/repo",
